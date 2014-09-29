831d20c60bfc0cbd7819d0e12050a271b56b93d0
jcbages <jcbages@outlook.com>
2014-09-29 15:41:13 -0500

    add the sql statements used in oracle

---
+++ SQL/project/iter2.sql
@@ -0,0 +1,63 @@
+CREATE TABLE genericuser (
+    login VARCHAR2(25) PRIMARY KEY,
+    password VARCHAR2(25) NOT NULL,
+    time_created TIMESTAMP NOT NULL
+);
+
+CREATE TABLE active (
+    login VARCHAR2(25) PRIMARY KEY,
+    passive VARCHAR2(25) NOT NULL,
+    FOREIGN KEY (login)
+    REFERENCES genericuser(login)
+    ON DELETE CASCADE,
+    FOREIGN KEY (passive)
+    REFERENCES passive(login)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE passive (
+    login VARCHAR2(25) NOT NULL UNIQUE,
+    register VARCHAR(25) PRIMARY KEY,
+    FOREIGN KEY (login)
+    REFERENCES genericuser(login)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE val (
+    pk_id INTEGER PRIMARY KEY,
+    name VARCHAR2(25) NOT NULL,
+    price INTEGER NOT NULL,
+    quantity INTEGER NOT NULL,
+    offerant VARCHAR2(25) NOT NULL
+);
+
+CREATE TABLE solicitude (
+    pk_id INTEGER PRIMARY KEY, 
+    operation_type VARCHAR2(1) NOT NULL,
+    val_pk INTEGER NOT NULL,
+    quantity INTEGER NOT NULL,
+    quantity_type VARCHAR2(1) NOT NULL,
+    time_created TIMESTAMP NOT NULL,
+    active_login VARCHAR2(25) NOT NULL,
+    solved VARCHAR2(1) NOT NULL,
+    is_active VARCHAR2(1) NOT NULL,
+    FOREIGN KEY (val)
+    REFERENCES val(pk_id)
+    ON DELETE CASCADE,
+    FOREIGN KEY (active_login)
+    REFERENCES active(login)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE swaptransaction (
+    pk_id INTEGER PRIMARY KEY,
+    time_created TIMESTAMP NOT NULL,
+    sell_solicitude INTEGER NOT NULL,
+    buy_solicitude INTEGER NOT NULL,
+    FOREIGN KEY (sell_solicitude)
+    REFERENCES solicitude(pk_id)
+    ON DELETE CASCADE,
+    FOREIGN KEY (buy_solicitude)
+    REFERENCES solicitude(pk_id)
+    ON DELETE CASCADE
+);
