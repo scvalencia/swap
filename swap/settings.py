2e308efbcdddf16919192420713bc4219c806e0f
jcbages <jcbages@outlook.com>
2014-09-29 23:48:27 -0500

    fixed settings.p, add secure cookies vals

--- swap/settings.py
+++ swap/settings.py
@@ -114,3 +114,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Session cookie domain and secure
+
+SESSION_COOKIE_DOMAIN = '.swap.herokuapp.com'
+SESSION_COOKIE_SECURE = False 
