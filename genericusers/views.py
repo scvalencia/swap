cdbb174c76e9928e9c5226e028e41b13eaf00542
jcbages <jcbages@outlook.com>
2014-09-29 19:34:38 -0500

    add the login logic

--- genericusers/views.py
+++ genericusers/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db import connection
 
-# Create your views here.
+import os
+import datetime
+
+
+def login(request):
+    if request.method == 'POST':
+        form_data = request.POST
+        valid, error = is_valid(form_data)
+        params = {'form_data': form_data, 'error', error}
+        if valid:
+            response = #INSERT RESPONSE
+            set_cookie(response, key, value, days_expire=7)
+        else:
+            return render(request, ) 
+    else:
+        form_data = {}
+        params = {'form_data': form_data}
+        return render(request, 'login.html', params)
+
+def validate_user(username, password):
+    # TODO: VALIDATE USER AND RETURN TRUE OR FALSE WITH THE RESPECTIVE ERROR
+
+def is_valid(form_data):
+    username = form_data.get('username')
+    password = form_data.get('password')
+    if username and password:
+        return validate_user(username, password)
+    elif not username and password:
+        return False, 'El nombre de usuario no puede estar vacio.'
+    elif username and not password:
+        return False, 'La clave no puede estar vacia.'
+    else:
+        return False, 'Los campos no pueden estar vacios.'
+
+def set_cookie(response, key, value, days_expire = 7):
+    if days_expire is None:
+        max_age = 365 * 24 * 60 * 60  #one year
+    else:
+        max_age = days_expire * 24 * 60 * 60 
+        expires = datetime.datetime.strftime(datetime.datetime.utcnow() +
+                  datetime.timedelta(seconds=max_age), "%a, %d-%b-%Y %H:%M:%S GMT")
+        domain = settings.SESSION_COOKIE_DOMAIN
+        secure = settings.SESSION_COOKIE_SECURE or None
+    response.set_cookie(key, value, max_age=max_age, expires=expires, domain=domain, secure=secure)
