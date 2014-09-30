38337eb6657397521cfe279b7acf977298fade48
jcbages <jcbages@outlook.com>
2014-09-30 18:37:03 -0500

    fix generiusers urls.py

--- genericusers/urls.py
+++ genericusers/urls.py
@@ -4,8 +4,8 @@ import views
 
 
 urlpatterns = patterns('',
-    url(r'^login/', views.login, name='login'),
-    url(r'^signup/', views.signup, name='signup'),
-    url(r'^logout/', views.logout, name='logout'),
-    url(r'^home/', views.home, name='home'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^logout/$', views.logout, name='logout'),
+    url(r'^home/$', views.home, name='home'),
 )
