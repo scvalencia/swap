2e4c579f2be10bccaf00d12af7b24c4bc26852aa
jcbages <jcbages@outlook.com>
2014-09-30 16:05:14 -0500

    set urls.py in genericusers

--- genericusers/urls.py
+++ genericusers/urls.py
@@ -5,6 +5,7 @@ import views
 
 urlpatterns = patterns('',
     url(r'^login/', views.login, name='login'),
+    url(r'^signup/', views.signup, name='signup'),
+    url(r'^logout/', views.logout, name='logout'),
     url(r'^home/', views.home, name='home'),
-    url(r'^signup', views.signup, name='signup')
 )
