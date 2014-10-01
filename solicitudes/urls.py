39f77c026ee7f51d387f0f98b43a24dd7421d40d
jcbages <jcbages@outlook.com>
2014-09-30 23:57:42 -0500

    modify solicitudes

---
+++ solicitudes/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+
+import views
+
+
+urlpatterns = patterns('',
+    url(r'^new_solicitude/$', views.new_solicitude, name='new_solicitude'),
+    url(r'^active_solicitudes/$', views.active_solicitudes, name='active_solicitudes'),
+    url(r'^passive_pending_solicitudes/$', views.passive_pending_solicitudes, name='passive_pending_solicitudes'),
+    url(r'^passive_solicitudes/$', views.passive_solicitudes, name='passive_solicitudes'),
+)
--- solicitudes/views.py
+++ solicitudes/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+######################## DJANGO IMPORTS ########################
+from django.db import connection
+from django.shortcuts import render, redirect
 
-# Create your views here.
+######################## CUSTOM IMPORTS ########################
+import genericuser
+import random, string
+
+
+################################################################
+######################## VIEW FUNCTIONS ########################
+################################################################
+
+
+def new_solicitude(request):
+	'''Returns the respective response to the new_solicitude url call.'''
+	username = check_username(request)
+	if not username:
+		return redirect('/users/home/')
+    if request.method == 'POST':
+    	valid, error = is_valid_new_solicitude(request.POST)
+    	params = {
+    		#TODO insert post form inputs
+    		'message': error,
+    	}
+    	if valid:
+    		params = {
+    			#TODO insert blank form inputs
+    			'message': 'Tu solicitud fue creada satisfactoriamente!',
+    		}
+    	return render(request, 'new_solicitude.html', params)
+    else:
+    	params = {
+    		#TODO insert blank form inputs
+    	}
+    	return render(request, 'new_solicitude', params)
+
+def active_solicitudes(request):
+    pass
+    #TODO
+
+def passive_pending_solicitudes(request):
+    pass
+    #TODO
+
+def passive_solicitudes(request):
+    pass
+    #TODO
+
+
+################################################################
+################## HIGH LEVEL AUX FUNCTIONS ####################
+################################################################
+
+
+def check_username(request):
+	username = request.session.get('username')
+	if username:
+		user = get_user(username)[0]
+		if user:
+			return username
+		else:
+			del request.session['username']
+			return None
+	else:
+		return None
+
+
+################################################################
+################### LOW LEVEL AUX FUNCTIONS ####################
+################################################################
+
