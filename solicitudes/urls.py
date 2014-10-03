9ba5d3fb85cb2b06ae18230ee99bd6a3a2eb12d1
jcbages <jcbages@outlook.com>
2014-10-03 00:24:25 -0500

    bugs and bugs

--- solicitudes/urls.py
+++ solicitudes/urls.py
@@ -8,5 +8,5 @@ urlpatterns = patterns('',
     url(r'^active_solicitudes/$', views.active_solicitudes, name='active_solicitudes'),
     url(r'^passive_pending_solicitudes/$', views.passive_pending_solicitudes, name='passive_pending_solicitudes'),
     url(r'^passive_solicitudes/$', views.passive_solicitudes, name='passive_solicitudes'),
-    url(r'^(d+/)$', views.solicitude, name='solicitude')
+    url(r'^(\d+)/$', views.solicitude, name='solicitude')
 )
