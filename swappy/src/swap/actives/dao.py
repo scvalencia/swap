ed85af72c67b9fcf16d812bcccec719ab45bd279
Sebastián Valencia <sc.valencia606@gmail.com>
2014-10-18 15:17:19 -0500

    Actives DAO

--- swappy/src/swap/actives/dao.py
+++ swappy/src/swap/actives/dao.py
@@ -1,97 +1,147 @@
+from resultset.resultSet import ResultSet
 from django.db import connection
 from models import ActiveDump
 from models import Active
 
-
 class ActiveDao(object):
     
     def __init__(self):
+        self.table_name = 'actives'
+        self.schema = ('user_login', 'available_money')
         self.cursor = connection.cursor()
+        self.rs = ResultSet()
 
     def find_all(self, test = False):
-    	ans = []
-    	if not test:			
-    		query = "SELECT * FROM actives ORDER BY user_login"
-    		self.cursor.execute(query)
-    		result_set = [item for item in self.cursor.fetchall()]
-    		for itm in result_set:
-    			ans.append(self.process_row(itm))
-    	else:
-    		objects = list(Active.objects.all())
-    		result_set = [item for item in objects]
-    		for itm in result_set:
-    			ans.append(self.process_active_object(itm))
-    	return ans
+        self.cursor = connection.cursor()
+        ans = []
+        if not test:
+            try:
+                query = "SELECT * FROM actives ORDER BY user_login"
+                self.cursor.execute(query)
+                result_set = [item for item in self.cursor.fetchall()]
+                for itm in result_set:
+                    ans.append(self.process_row(itm))
+            except Exception as e:
+                print e
+                return []
+        else:
+            try:
+                objects = list(Active.objects.all())
+                result_set = [item for item in objects]
+                for itm in result_set:
+                    ans.append(self.process_object(itm))
+            except Exception as e:
+                print e
+                return []
+        self.rs.set(ans)
+        return ans
 
-    def find_by_login(self, login, test = False):
-    	login = str(login)
-    	ans = []
-    	if not test:
-    		query = "SELECT * FROM actives WHERE user_login = %s"
-    		params = [login]
-    		self.cursor.execute(query, params)
-    		result_set = [item for item in self.cursor.fetchall()]
-    		for itm in result_set:
-    			ans.append(self.process_row(itm))
-    	else:
-    		objects = list(Active.objects.filter(user_login = login))
-    		result_set = [item for item in objects]
-    		for itm in result_set:
-    			ans.append(self.process_active_object(itm))
-    	return ans
+    def find_by_login(self, arg_login, test = False):
+        login = str(arg_login)
+        ans = []
+        if not test:
+            try:
+                query = "SELECT * FROM actives WHERE user_login = %s"
+                params = [login]
+                self.cursor.execute(query, params)
+                result_set = [item for item in self.cursor.fetchall()]
+                for itm in result_set:
+                    ans.append(self.process_row(itm))
+            except Exception as e:
+                print e                
+                return []
+        else:
+            try:
+                objects = list(Active.objects.filter(user_login = login))
+                result_set = [item for item in objects]
+                for itm in result_set:
+                    ans.append(self.process_object(itm))
+            except Exception as e:
+                print e                
+                return []
+        self.rs.set(ans)        
+        return ans
 
-    def find_by_money(self, money, test = False):
-    	money = float(money)
-    	ans = []
-    	if not test:
-    		query = "SELECT * FROM actives WHERE available_money = %s"
-    		params = [money]
-    		self.cursor.execute(query, params)
-    		result_set = [item for item in self.cursor.fetchall()]
-    		for itm in result_set:
-    			ans.append(self.process_row(itm))
-    	else:
-    		objects = list(Active.objects.filter(available_money = money))
-    		result_set = [item for item in objects]
-    		for itm in result_set:
-    			ans.append(self.process_active_object(itm))
-    	return ans
-
-    def insert(self, arg_user_login, arg_available_money, test = False):
-    	user_login = str(arg_user_login)
-    	available_money = str(arg_available_money)
-    	if not test:
-    		active_object = ActiveDump(user_login, available_money)
-    		query = "INSERT INTO actives VALUES(%s, %s)"
-    		params = [user_login, available_money]
-    		self.cursor.execute(query, params)
-    	else:
-    		active_object = Active(user_login = arg_user_login, 
-    			available_money = float(arg_available_money))
-    		active_object.save()
-
-    def create(self, active_object, test = False):
-    	user_login = active_object.user_login
-    	available_money = active_object.available_money
-    	self.insert(user_login, available_money, test)
+    def insert(self, arg_login, arg_money, test = False):
+        login = str(arg_login)
+        money = str(arg_money)
+        if not test:
+            try:
+                query = "INSERT INTO actives VALUES(%s, %s)"
+                params = [login, money]
+                self.cursor.execute(query, params)                
+                return True
+            except Exception as e:
+                print e               
+                return False
+        else:
+            try:
+                generic_object = Active(login = login, user_id = user_id)
+                generic_object.save()                
+                return True
+            except Exception as e:
+                print e                
+                return False
 
     def update(self, active_object, test = False):
-    	pass
+        login = active_object.user_login
+        money = active_object.available_money
+        objects = self.find_by_login(login)
+        available_for_update = (len(objects) == 1)
+        if(available_for_update):
+            if not test:
+                try:
+                    query = ("UPDATE actives "
+                             "SET available_money = %s "
+                             "WHERE user_login = %s")
+                    params = [money, login]
+                    self.cursor.execute(query, params)                    
+                    return True
+                except Exception as e:
+                    print e                    
+                    return False
+            else:
+                try:
+                    one_object = Active.objects.filter(user_login = login).update(available_money = money)                    
+                    return True
+                except Exception as e:
+                    print e                    
+                    return False
+        else:
+            return False
 
-    def save(self, active_object, test = False):
-    	pass
+    def remove(self, login, test = False):
+        login = str(login)
+        objects = self.find_by_login(login)
+        available_for_update = (len(objects) == 1)
+        if(available_for_update):
+            if not test:
+                try:
+                    query = ("DELETE FROM actives WHERE user_login = %s")
+                    params = [login]
+                    self.cursor.execute(query, params)                    
+                    return True
+                except Exception as e:
+                    print e                    
+                    return False
+            else:
+                try:
+                    Active.objects.filter(user_login = login).delete()                    
+                    return True
+                except Exception as e:
+                    print e                    
+                    return False
+        else:            
+            return False
 
-    def remove(self, active_object, test = False):
-    	pass
-
-    def process_active_object(self, active_object):
-    	user_login = active_object.user_login
-    	available_money = active_object.available_money
-    	active_dump = ActiveDump(user_login, available_money)
-    	return active_dump
+    def process_object(self, generic_object):
+        login = generic_object.user_login
+        money = generic_object.available_money
+        dump = ActiveDump(login, money)
+        return dump
 
     def process_row(self, result_set):
-    	user_login = result_set[0]
-    	available_money = result_set[1]
-    	active_object = ActiveDump(user_login, available_money)
-    	return active_object
+        login = result_set[0]
+        money = result_set[1]
+        generic_object = ActiveDump(login, money)
+        return generic_object
