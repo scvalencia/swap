054d45c5c7ce3c0a323b565b528b7466bbbd7c2f
Sebastián Valencia <sc.valencia606@gmail.com>
2014-10-18 18:32:43 -0500

    Val model

--- swappy/src/swap/vals/models.py
+++ swappy/src/swap/vals/models.py
@@ -38,3 +38,49 @@ class Val(models.Model):
     class Meta:
         db_table = 'vals'
         managed = False
+
+class RentDump(object):
+
+    def __init__(self, pk_id, rent_name, desc, rent_function, rent_len, rent_tp, login):
+        self.pk_id = pk_id
+        self.rent_name = rent_name
+        self.description = desc
+        self.rent_function = rent_function
+        self.rent_length = rent_len
+        self.rent_type = rent_tp
+        self.offerant_login = login
+
+    def __str__(self):
+        ans = '('
+        ans += str(self.pk_id) + ', '
+        ans += str(self.rent_name) + ', '
+        ans += str(self.description) + ', '
+        ans += str(self.rent_function) + ', '
+        ans += str(self.rent_length) + ', '
+        ans += str(self.rent_type) + ', '
+        ans += str(self.offerant_login)
+        ans += ')'
+        return ans
+
+class ValDump(object):
+
+    def __init__(self, pk, name, desc, val_type, amount, price, rent_id):
+        self.pk_id = pk
+        self.val_name = name
+        self.description = desc
+        self.val_type = val_type
+        self.amount = amount
+        self.price = price
+        self.rent_id = rent_id
+
+    def __str__(self):
+        ans = '('
+        ans += str(self.pk_id) + ', '
+        ans += str(self.val_name) + ', '
+        ans += str(self.description) + ', '
+        ans += str(self.val_type) + ', '
+        ans += str(self.amount) + ', '
+        ans += str(self.price) + ', '
+        ans += str(self.rent_id)
+        ans += ')'
+        return ans
