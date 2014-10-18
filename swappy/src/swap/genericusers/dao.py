b0eb016ea1a16d64fd1696fbcbc846987a28d8df
Sebastián Valencia <sc.valencia606@gmail.com>
2014-10-18 15:17:44 -0500

    Users DAO

--- swappy/src/swap/genericusers/dao.py
+++ swappy/src/swap/genericusers/dao.py
@@ -8,6 +8,9 @@ from resultset.resultSet import ResultSet
 class GenericUserDao(object):
 
     def __init__(self):
+        self.table_name = 'users'
+        self.schema = ('login', 'user_id', 'first_name', 'last_name', 
+            'email', 'phone', 'user_pass')
         self.cursor = connection.cursor()
         self.rs = ResultSet()
 
@@ -27,7 +30,7 @@ class GenericUserDao(object):
         		objects = list(GenericUser.objects.all())
         		result_set = [item for item in objects]
         		for itm in result_set:
-        			ans.append(self.process_generic_user_object(itm))
+        			ans.append(self.process_object(itm))
             except:
                 return []
         self.rs.set(ans)
@@ -51,130 +54,10 @@ class GenericUserDao(object):
                 objects = list(GenericUser.objects.filter(login = login))
                 result_set = [item for item in objects]
                 for itm in result_set:
-                    ans.append(self.process_generic_user_object(itm))
+                    ans.append(self.process_object(itm))
             except:
                 return []
-        self.rs.set(ans)
-        return ans
-
-    def find_by_id(self, arg_id, test = False):
-        arg_id = str(arg_id)
-        ans = []
-        if not test:
-            try:
-                query = "SELECT * FROM users WHERE user_id = %s"
-                params = [arg_id]
-                self.cursor.execute(query, params)
-                result_set = [item for item in self.cursor.fetchall()]
-                for itm in result_set:
-                    ans.append(self.process_row(itm))
-            except:
-                return []
-        else:
-            try:
-                objects = list(GenericUser.objects.filter(user_id = arg_id))
-                result_set = [item for item in objects]
-                for itm in result_set:
-                    ans.append(self.process_generic_user_object(itm))
-            except:
-                return []
-        self.rs.set(ans)
-        return ans
-
-    def find_by_first_name(self, first_name, test = False):
-        first_name = str(first_name)
-        ans = []
-        if not test:
-            try:
-                query = "SELECT * FROM users WHERE first_name = %s"
-                params = [first_name]
-                self.cursor.execute(query, params)
-                result_set = [item for item in self.cursor.fetchall()]
-                for itm in result_set:
-                    ans.append(self.process_row(itm))
-            except:
-                return []
-        else:
-            try:
-                objects = list(GenericUser.objects.filter(first_name = first_name))
-                result_set = [item for item in objects]
-                for itm in result_set:
-                    ans.append(self.process_generic_user_object(itm))
-            except:
-                return []
-        self.rs.set(ans)
-        return ans
-
-    def find_by_last_name(self, last_name, test = False):
-        last_name = str(last_name)
-        ans = []
-        if not test:
-            try:
-                query = "SELECT * FROM users WHERE last_name = %s"
-                params = [last_name]
-                self.cursor.execute(query, params)
-                result_set = [item for item in self.cursor.fetchall()]
-                for itm in result_set:
-                    ans.append(self.process_row(itm))
-            except:
-                return []
-        else:
-            try:
-                objects = list(GenericUser.objects.filter(last_name = last_name))
-                result_set = [item for item in objects]
-                for itm in result_set:
-                    ans.append(self.process_generic_user_object(itm))
-            except:
-                return []
-        self.rs.set(ans)
-        return ans
-
-    def find_by_email(self, email, test = False):
-        email = str(email)
-        ans = []
-        if not test:
-            try:
-                query = "SELECT * FROM users WHERE email = %s"
-                params = [email]
-                self.cursor.execute(query, params)
-                result_set = [item for item in self.cursor.fetchall()]
-                for itm in result_set:
-                    ans.append(self.process_row(itm))
-            except:
-                return []
-        else:
-            try:
-                objects = list(GenericUser.objects.filter(email = email))
-                result_set = [item for item in objects]
-                for itm in result_set:
-                    ans.append(self.process_generic_user_object(itm))
-            except:
-                return []
-        self.rs.set(ans)
-        return ans
-
-    def find_by_phone(self, phone, test = False):
-        phone = str(phone)
-        ans = []
-        if not test:
-            try:
-                query = "SELECT * FROM users WHERE phone = %s"
-                params = [phone]
-                self.cursor.execute(query, params)
-                result_set = [item for item in self.cursor.fetchall()]
-                for itm in result_set:
-                    ans.append(self.process_row(itm))
-            except:
-                return []
-        else:
-            try:
-                objects = list(GenericUser.objects.filter(phone = phone))
-                result_set = [item for item in objects]
-                for itm in result_set:
-                    ans.append(self.process_generic_user_object(itm))
-            except:
-                return []
-        self.rs.set(ans)
+        self.rs.set(ans)        
         return ans
     
     def insert(self, arg_login, arg_user_id, arg_user_pass, arg_first_name, 
@@ -190,32 +73,75 @@ class GenericUserDao(object):
             try:
                 query = "INSERT INTO users VALUES(%s, %s, %s, %s, %s, %s, %s)"
                 params = [login, user_id, user_pass, first_name, last_name, email, phone]
-                self.cursor.execute(query, params)
+                self.cursor.execute(query, params)                
                 return True
-            except:
+            except:                
                 return False
         else:
             try:
                 generic_user_object = GenericUser(login = login, user_id = user_id, user_pass = user_pass,
                     first_name = first_name, last_name = last_name, email = email, phone = phone)
-                generic_user_object.save()
+                generic_user_object.save()                
                 return True
-            except:
+            except:                
                 return False
 
-    def create(self, active_object, test = False):
-        ans = self.insert(active_object.login, active_object.user_id, 
-            active_object.user_pass, active_object.first_name, active_object.last_name,
-            active_object.email, active_object.phone, test)
-        return ans
+    def update(self, user_object, test = False):
+        login = user_object.login
+        user_id = user_object.user_id
+        user_pass = user_object.user_pass
+        first_name = user_object.first_name
+        last_name = user_object.last_name
+        email = user_object.email
+        phone = user_object.phone
+        objects = self.find_by_login(login)
+        available_for_update = (len(objects) == 1)
+        if(available_for_update):
+            if not test:
+                try:
+                    query = ("UPDATE users "
+                             "SET user_id = %s, user_pass = %s, first_name = %s, last_name = %s, "
+                             "email = %s, phone = %s "
+                             "WHERE login = %s")
+                    params = [user_id, user_pass, first_name, last_name, email, phone, login]
+                    self.cursor.execute(query, params)                    
+                    return True
+                except:                    
+                    return False
+            else:
+                try:
+                    one_object = GenericUser.objects.filter(login = login).update(user_id = user_id,
+                        user_pass = user_pass, first_name = first_name, last_name = last_name,
+                        email = email, phone = phone)                    
+                    return True
+                except:                    
+                    return False
+        else:
+            return False
 
-    def update(self, active_object, test = False):
-        pass
+    def remove(self, login, test = False):
+        login = str(login)
+        objects = self.find_by_login(login)
+        available_for_update = (len(objects) == 1)
+        if(available_for_update):
+            if not test:
+                try:
+                    query = ("DELETE FROM users WHERE login = %s")
+                    params = [login]
+                    self.cursor.execute(query, params)                    
+                    return True
+                except:                    
+                    return False
+            else:
+                try:
+                    GenericUser.objects.filter(login = login).delete()                    
+                    return True
+                except:                    
+                    return False
+        else:            
+            return False
 
-    def remove(self, active_object, test = False):
-        pass
-
-    def process_generic_user_object(self, generic_user_object):
+    def process_object(self, generic_user_object):
         login = generic_user_object.login
         user_id = generic_user_object.user_id
         user_pass = generic_user_object.user_pass
@@ -242,4 +168,158 @@ class GenericUserDao(object):
 class LegalDao(object):
 
     def __init__(self):
-        aself.cursor = connection.cursor()
+        self.table_name = 'legals'
+        self.schema = ('legal_id', 'legal_name', 'user_login')
+        self.cursor = connection.cursor()
+        self.rs = ResultSet()
+
+    def find_all(self, test = False):
+        ans = []
+        if not test:
+            try:            
+                query = "SELECT * FROM legals ORDER BY login"
+                self.cursor.execute(query)
+                result_set = [item for item in self.cursor.fetchall()]
+                for itm in result_set:
+                    ans.append(self.process_row(itm))
+            except:
+                return []
+        else:
+            try:
+                objects = list(GenericUser.objects.all())
+                result_set = [item for item in objects]
+                for itm in result_set:
+                    ans.append(self.process_object(itm))
+            except:
+                return []
+        self.rs.set(ans)
+        return ans
+
+    def find_by_login(self, arg_login, test = False):
+        login = str(arg_login)
+        ans = []
+        if not test:
+            try:
+                query = "SELECT * FROM users WHERE login = %s"
+                params = [login]
+                self.cursor.execute(query, params)
+                result_set = [item for item in self.cursor.fetchall()]
+                for itm in result_set:
+                    ans.append(self.process_row(itm))
+            except:
+                return []
+        else:
+            try:
+                objects = list(GenericUser.objects.filter(login = login))
+                result_set = [item for item in objects]
+                for itm in result_set:
+                    ans.append(self.process_object(itm))
+            except:
+                return []
+        self.rs.set(ans)        
+        return ans
+    
+    def insert(self, arg_login, arg_user_id, arg_user_pass, arg_first_name, 
+        arg_last_name, arg_email, arg_phone, test = False):
+        login = str(arg_login)
+        user_id = str(arg_user_id)
+        user_pass = str(arg_user_pass)
+        first_name = str(arg_first_name)
+        last_name = str(arg_last_name)
+        email = str(arg_email)
+        phone = str(arg_phone)
+        if not test:
+            try:
+                query = "INSERT INTO users VALUES(%s, %s, %s, %s, %s, %s, %s)"
+                params = [login, user_id, user_pass, first_name, last_name, email, phone]
+                self.cursor.execute(query, params)                
+                return True
+            except:                
+                return False
+        else:
+            try:
+                generic_user_object = GenericUser(login = login, user_id = user_id, user_pass = user_pass,
+                    first_name = first_name, last_name = last_name, email = email, phone = phone)
+                generic_user_object.save()                
+                return True
+            except:                
+                return False
+
+    def update(self, user_object, test = False):
+        login = user_object.login
+        user_id = user_object.user_id
+        user_pass = user_object.user_pass
+        first_name = user_object.first_name
+        last_name = user_object.last_name
+        email = user_object.email
+        phone = user_object.phone
+        objects = self.find_by_login(login)
+        available_for_update = (len(objects) == 1)
+        if(available_for_update):
+            if not test:
+                try:
+                    query = ("UPDATE users "
+                             "SET user_id = %s, user_pass = %s, first_name = %s, last_name = %s, "
+                             "email = %s, phone = %s "
+                             "WHERE login = %s")
+                    params = [user_id, user_pass, first_name, last_name, email, phone, login]
+                    self.cursor.execute(query, params)                    
+                    return True
+                except:                    
+                    return False
+            else:
+                try:
+                    one_object = GenericUser.objects.filter(login = login).update(user_id = user_id,
+                        user_pass = user_pass, first_name = first_name, last_name = last_name,
+                        email = email, phone = phone)                    
+                    return True
+                except:                    
+                    return False
+        else:
+            return False
+
+    def remove(self, login, test = False):
+        login = str(login)
+        objects = self.find_by_login(login)
+        available_for_update = (len(objects) == 1)
+        if(available_for_update):
+            if not test:
+                try:
+                    query = ("DELETE FROM users WHERE login = %s")
+                    params = [login]
+                    self.cursor.execute(query, params)                    
+                    return True
+                except:                    
+                    return False
+            else:
+                try:
+                    GenericUser.objects.filter(login = login).delete()                    
+                    return True
+                except:                    
+                    return False
+        else:            
+            return False
+
+    def process_object(self, generic_user_object):
+        login = generic_user_object.login
+        user_id = generic_user_object.user_id
+        user_pass = generic_user_object.user_pass
+        first_name = generic_user_object.first_name
+        last_name = generic_user_object.last_name
+        email = generic_user_object.email
+        phone = generic_user_object.phone
+        generic_user_dump = GenericUserDump(login, user_id, user_pass, 
+            first_name, last_name, email, phone)
+        return generic_user_dump
+
+    def process_row(self, result_set):
+        login = result_set[0]
+        user_id = result_set[1]
+        first_name = result_set[2]
+        last_name = result_set[3]
+        email = result_set[4]
+        phone = result_set[5]
+        user_pass = result_set[6]
+        generic_user_object = GenericUserDump(login, user_id, user_pass, 
+            first_name, last_name, email, phone)
+        return generic_user_object
