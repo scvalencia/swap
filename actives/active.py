2cd465f28d66c5bcaa3a5a04221751053d7a24b6
Sebastián Valencia <sc.valencia606@gmail.com>
2014-10-19 11:12:19 -0500

    Filesystems

--- actives/active.py
+++
@@ -1,4 +0,0 @@
-class Active(object):
-    def __init__(self, login, passive):
-        self.login = login
-        self.passive = passive
--- passives/passive.py
+++
@@ -1,7 +0,0 @@
-class Passive(object):
-    def __init__(self, login, register):
-        self.login = login
-        self.register = register
-
-    def __hash__(self):
-    	return self.login
